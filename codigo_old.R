eaf02f44c0b0d18694b0dde689b3817f1fbf363d
Gabriel Maia <gabmaia23@gmail.com>
2021-06-20 13:44:48 -0300

    a

--- codigo_old.R
+++ codigo_old.R
@@ -5,46 +5,60 @@ library(tidyverse)
 
 dados <- readr::read_csv("dados.csv")
 
+
+
+
 dados %>% 
+  filter(id!="P48") %>% 
+  pivot_longer(-id, names_to=c(".value", "empresa"), names_pattern="([^_]+)_(.*)") %>% 
+  filter(!is.na(empresa)) %>% mutate(taxa_escolhas=escolhas/12, taxa_escolhas=replace_na(taxa_escolhas, 0))->escolhas
+
+dados %>%  
+  filter(id!="P48") %>% 
+  pivot_longer(-id, names_to=c(".value", "empresa"), names_pattern="([^_]+)_(.*)") %>% 
+  filter(!is.na(empresa)) %>% mutate(taxa_sucesso=ganhos/escolhas, taxa_sucesso=replace_na(taxa_sucesso, 0),
+                                     avaliação=replace_na(avaliação, 0))->sucesso
+
+
+cor.test(escolhas$taxa_escolhas,escolhas$avaliação, method = "pearson")->a
+
+
+dados %>% 
+  filter(id!="P48") %>% 
   pivot_longer(-c("id","Coleta"), names_to=c(".value", "empresa"), names_pattern="([^_]+)_(.*)") %>% 
   filter(!is.na(empresa)) %>% mutate(taxa_sucesso=ganhos/escolhas, taxa_sucesso=replace_na(taxa_sucesso, 0))%>% 
-  ggplot(aes(x=escolhas, y=avaliação,col=Coleta))+
-  geom_point()
+  ggplot()+
+  geom_point(aes(x=escolhas, y=avaliação,shape=empresa),position=position_dodge(width = 0.20))+
+  geom_smooth(aes(x=escolhas, y=avaliação), method = "lm", se = F, col="black")+
+  geom_text(data=data_frame(x=9, y=1),
+            aes(x=x, y=y, label=paste0("R = ",round(a$estimate,2),", p = ", 
+                                       formatC(a$p.value, format="e", digits = 2 ))))
 
 
-
-dados %>% 
-  filter(id!="P24") %>% 
-  pivot_longer(-id, names_to=c(".value", "empresa"), names_pattern="([^_]+)_(.*)") %>% 
-  filter(!is.na(empresa)) %>% mutate(taxa_sucesso=ganhos/escolhas, taxa_sucesso=replace_na(taxa_sucesso, 0))->bob
-
-chisq.test(bob$escolhas,bob$avaliação)
-
-cor.test(bob$taxa_sucesso,bob$avaliação, method = "pearson")
-
-library(ggpubr)
-
-ggqqplot(bob$escolhas)
-
-bob %>% ggscatter(x="taxa_sucesso", y="avaliação", add = "reg.line", conf.int = F, 
-                  cor.coef = TRUE, cor.method = "pearson")
+ext.functions::extplot( "Avaliação das empresas versus número de escolhas",1,0999,
+                       theme=ggthemes::theme_clean())
 
 
 
 
+
+
+
+
+cor.test(sucesso$taxa_sucesso,sucesso$avaliação, method = "pearson")->b
 
 
 dados %>% 
-  filter(id!="P24") %>% 
-  pivot_longer(-id, names_to=c(".value", "empresa"), names_pattern="([^_]+)_(.*)") %>% 
-  filter(!is.na(empresa)) %>% mutate(taxa_escolhas=escolhas/12, taxa_escolhas=replace_na(taxa_escolhas, 0))->bob2
+  filter(id!="P48") %>% 
+  pivot_longer(-c("id","Coleta"), names_to=c(".value", "empresa"), names_pattern="([^_]+)_(.*)") %>% 
+  filter(!is.na(empresa)) %>% mutate(taxa_sucesso=ganhos/escolhas, taxa_sucesso=replace_na(taxa_sucesso, 0))%>% 
+  ggplot()+
+  geom_point(aes(x=taxa_sucesso, y=avaliação,shape=empresa),position=position_dodge(width = 0.20))+
+  geom_smooth(aes(x=taxa_sucesso, y=avaliação), method = "lm", se = F, col="black")+
+  geom_text(data=data_frame(x=0.7, y=1.8),
+            aes(x=x, y=y, label=paste0("R = ",round(b$estimate,2),", p = ", 
+                                       formatC(b$p.value, format="e", digits = 2 ))))
 
-chisq.test(bob2$escolhas,bob$avaliação)
+ext.functions::extplot( "Avaliação das empresas versus taxa de sucesso",2,0999,
+                        theme=ggthemes::theme_clean())
 
-cor.test(bob2$taxa_escolhas,bob$avaliação, method = "pearson")
-
-
-ggqqplot(bob2$escolhas)
-
-bob2 %>% ggscatter(x="taxa_escolhas", y="avaliação", add = "reg.line", conf.int = F, 
-                  cor.coef = TRUE, cor.method = "pearson")
