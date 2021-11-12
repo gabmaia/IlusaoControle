8b481ca865b786feb258fe9ecb01860f6f915c67
Gabriel Maia <gabmaia23@gmail.com>
2021-11-12 17:47:04 -0300

    a

--- codigo_old.R
+++ codigo_old.R
@@ -29,11 +29,10 @@ sucesso %>%
   cbind(taxa_sucesso_norm=qnorm((rank(sucesso$taxa_sucesso, na.last = "keep") - 0.5) / sum(!is.na(sucesso$taxa_sucesso))))%>% 
   cbind(avaliação_norm=qnorm((rank(sucesso$avaliação, na.last = "keep") - 0.5) / sum(!is.na(sucesso$avaliação))))->sucesso
 
-cor.test(escolhas$taxa_escolhas_norm,escolhas$avaliação_norm, method = "pearson")
 
 
 
-cor.test(escolhas$taxa_escolhas_norm,escolhas$avaliação_norm, method = "pearson")->a
+cor.test(escolhas$escolhas,escolhas$avaliação, method = "spearman")->a
 
 
 
@@ -52,41 +51,75 @@ dados %>%
   cbind(taxa_escolhas_norm=qnorm((rank(escolhas$escolhas, na.last = "keep") - 0.5) / sum(!is.na(escolhas$escolhas))))%>% 
   cbind(avaliação_norm=qnorm((rank(escolhas$avaliação, na.last = "keep") - 0.5) / sum(!is.na(escolhas$avaliação)))) %>% 
   ggplot()+
-  geom_point(aes(x=taxa_escolhas_norm, y=avaliação_norm,shape=empresa),position=position_dodge(width = 0.20))+
-  geom_smooth(aes(x=taxa_escolhas_norm, y=avaliação_norm), method = "lm", se = F, col="black")+
-  geom_text(data=data_frame(x=0.8, y=-1.2),
-            aes(x=x, y=y, label=paste0("R = ",round(a$estimate,2),", p = ", 
+  geom_point(aes(x=escolhas/12, y=avaliação,shape=empresa),position=position_dodge(width = 0.040), size=3)+
+  geom_smooth(aes(x=escolhas/12, y=avaliação), method = "lm", se = F, col="black")+
+  geom_text(data=data_frame(x=0.8, y=2),
+            aes(x=x, y=y, label=paste0("ρ = ",round(a$estimate,2),
+                                          ", p = ", 
                                        formatC(a$p.value, format="e", digits = 2 ))))+
-  xlab("Taxa de escolhas (normalizada)")+
-  ylab("Avaliação (normalizada)")
+  xlab("Taxa de Escolhas")+
+  ylab("Avaliação")+
+  scale_size(limits = c(2,4))+
+  scale_shape_manual(values=1:2)
 
 
-ext.functions::extplot( "Avaliação das empresas versus número de escolhas",1,0999,
-                       theme=ggthemes::theme_clean())
-
-
-
-
-
+ext.functions::extplot( "Avaliação das empresas versus número de escolhas",3,0999,
+                       theme=ggthemes::theme_clean(),w = 8, h=6)
 
 
 
-cor.test(sucesso$taxa_sucesso_norm,sucesso$avaliação_norm, method = "pearson")->b
+
+
+
+
+
+cor.test(sucesso$taxa_sucesso,sucesso$avaliação, method = "spearman")->b
 
 
 dados %>% 
   pivot_longer(-c("id","Coleta"), names_to=c(".value", "empresa"), names_pattern="([^_]+)_(.*)") %>% 
-  cbind(taxa_sucesso_norm=qnorm((rank(sucesso$taxa_sucesso, na.last = "keep") - 0.5) / sum(!is.na(sucesso$taxa_sucesso))))%>% 
-  cbind(avaliação_norm=qnorm((rank(sucesso$avaliação, na.last = "keep") - 0.5) / sum(!is.na(sucesso$avaliação)))) %>% 
+  cbind(taxa_sucesso_norm=sucesso$taxa_sucesso)%>% 
+  cbind(avaliação_norm=(sucesso$avaliação)) %>% 
   ggplot()+
-  geom_point(aes(x=taxa_sucesso_norm, y=avaliação_norm,shape=empresa),position=position_dodge(width = 0.20))+
+  geom_point(aes(x=taxa_sucesso_norm, y=avaliação_norm,shape=empresa),position=position_dodge(width = 0.040), size=3)+
   geom_smooth(aes(x=taxa_sucesso_norm, y=avaliação_norm), method = "lm", se = F, col="black")+
-  geom_text(data=data_frame(x=0.7, y=-1.4),
-            aes(x=x, y=y, label=paste0("R = ",round(b$estimate,2),", p = ", 
+  geom_text(data=data_frame(x=0.8, y=2.0),
+            aes(x=x, y=y, label=paste0("ρ = ",round(b$estimate,2),
+                                       ", p = ", 
                                        formatC(b$p.value, format="e", digits = 2 ))))+
-  xlab("Taxa de Sucesso (normalizada)")+
-  ylab("Avaliação (normalizada)")
+  xlab("Taxa de Sucesso")+
+  ylab("Avaliação")+
+  scale_size(limits = c(2,4))+
+  scale_shape_manual(values=1:2)
+  
 
 ext.functions::extplot( "Avaliação das empresas versus taxa de sucesso",2,0999,
-                        theme=ggthemes::theme_clean())
+                        theme=ggthemes::theme_clean(),w = 8, h=6)
+
+
+
+
+
+cor.test(escolhas$taxa_escolhas, sucesso$taxa_sucesso,method = "spearman")->c
+
+
+dados %>% 
+  pivot_longer(-c("id","Coleta"), names_to=c(".value", "empresa"), names_pattern="([^_]+)_(.*)") %>% 
+  cbind(taxa_sucesso_norm=sucesso$taxa_sucesso)%>% 
+  cbind(avaliação_norm=(escolhas$taxa_escolhas)) %>% 
+  ggplot()+
+  geom_point(aes(x=taxa_sucesso_norm, y=avaliação_norm,shape=empresa),position=position_dodge(width = 0.040), size=3)+
+  geom_smooth(aes(x=taxa_sucesso_norm, y=avaliação_norm), method = "lm", se = F, col="black")+
+  geom_text(data=data_frame(x=0.8, y=0.0),
+            aes(x=x, y=y, label=paste0("ρ = ",round(c$estimate,2),
+                                       ", p = ", 
+                                       formatC(c$p.value, format="e", digits = 2 ))))+
+  xlab("Taxa de Sucesso")+
+  ylab("Taxa da Escolha")+
+  scale_size(limits = c(2,4))+
+  scale_shape_manual(values=1:2)
+
+
+ext.functions::extplot( "Taxa de escolhas das empresas versus taxa de sucesso",1,0999,
+                        theme=ggthemes::theme_clean(),w = 8, h=6)
 
